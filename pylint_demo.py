48458743228829c88aeab6c8715c27a6b4432377
Minseok Kim <kms0813gg@gmail.com>
2025-03-03 15:20:36 +0400

    fixed pylint faults

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,19 @@
-def sum(a, b):
+"""
+docstring
+"""
+def sum1(a, b):
+    """
+    return the sum
+    """
     return a + b
 
 
 def hello(name):
+    """
+    Print a greeting message
+    """
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+hello("Minseok")
